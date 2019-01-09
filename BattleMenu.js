2260e106b8938aef7d86710337a7b4be485eea1a
abelhabel <anteolsson@gmail.com>
2019-01-09 14:38:06 -0500

    Closes #3

--- BattleMenu.js
+++ BattleMenu.js
@@ -13,6 +13,8 @@ class BattleMenu {
       abilities: null,
       default: null
     };
+    this.tw = 32;
+    this.th = 32;
     this.selected = null;
   }
 
@@ -40,21 +42,58 @@ class BattleMenu {
       } else {
         canvas.style.border = 'none';
       }
+      this.addTollTip(canvas, a.bio.name);
       this.tags.abilities.appendChild(canvas);
+    })
+  }
+
+  addTollTip(canvas, tip) {
+    let t;
+    canvas.addEventListener('mouseenter', e => {
+      let bb = canvas.getBoundingClientRect();
+      t = html`<div style='
+        position: fixed;
+        top: ${bb.y + 3}px;
+        left: ${bb.x + this.tw + 2}px;
+        color: black;
+        z-index: 1010;
+        pointer-events: none;
+        padding: 4px;
+        background-image: url(sheet_of_old_paper.png);
+        background-repeat: no-repeat;
+        overflow: hidden;
+        border-radius: 2px;
+        box-shadow: 0px 0px 5px 1px rgba(0,0,0,0.75);
+        '
+      >
+        ${tip}
+      </div>`;
+      document.body.appendChild(t);
+    });
+    canvas.addEventListener('mouseout', e => {
+      t && document.body.removeChild(t);
+      t = null;
+    })
+    canvas.addEventListener('mousedown', e => {
+      t && document.body.removeChild(t);
+      t = null;
     })
   }
 
   render() {
     let ability = new Sprite(_ability.bio.sprite);
+    this.addTollTip(ability.canvas, 'Character Sheet');
     ability.canvas.addEventListener('click', () => {
       this.battle.toggleAbilityBook();
     });
     let defend = new Sprite(_defend.bio.sprite);
+    this.addTollTip(defend.canvas, 'Defend');
     defend.canvas.addEventListener('click', e => {
       let action = this.battle.createAction({type: 'defend'});
       this.battle.addAction(action);
     });
     let wait = new Sprite(_wait.bio.sprite);
+    this.addTollTip(wait.canvas, 'Wait');
     wait.canvas.addEventListener('click', e => {
       let action = this.battle.createAction({type: 'wait'});
       this.battle.addAction(action);
